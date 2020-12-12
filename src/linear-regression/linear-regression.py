ba7ed6a3d2a76c14ea58a25f2c73b0878070b78f
bernardgonzales <bernard.luke.gonzales@gmail.com>
2020-12-12 01:42:37 -0800

    Refactored the linear regression to work on the pandas dataframe rather than np arrays. Updated the PDF test run.

--- src/linear-regression/linear-regression.py
+++ src/linear-regression/linear-regression.py
@@ -1,30 +1,17 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
-import matplotlib.pyplot as plt
+from sklearn import metrics
+from sklearn.metrics import accuracy_score
 
 # Import data set csv
 data_set = pd.read_csv('filtered_data_set.csv')
 
-# Convert to np array
-data_set = np.array(data_set)
-
-# Determine the number of rows in the raw data.
-num_rows = np.size(data_set, 0)
-
-# Using the number of rows, create a column of 1s to place as the first column in the data set.
-ones_col = (np.ones((num_rows, 1)))
-
-# Set X to all values to our inputs.
-X = np.concatenate((ones_col, data_set), axis=1)
-
-# Set y to only the output column
-y = data_set[:,-1]
-
-# Drop the last column
-X = X[:, :-1] 
+# Set X and y columns
+X = data_set[['valence','acousticness','danceability','duration_ms','energy','explicit',
+             'instrumentalness','key','liveness','loudness','mode','speechiness','tempo']]
+y = data_set['popularity']
 
 # Create the X training and testing set, and Y training and testing set where 70% of the rows
 # are for the training set and the rest to the testing set.
@@ -48,12 +35,33 @@ for i in range(0,len(predictions)):
     else:
         scaled_predictions.append(0)    
 
-# Calculate the accuracy
-correct = 0
-total = 0
-for i in range(0, len(predictions)):
-    if(y_testing_set[i] == scaled_predictions[i]):
-        correct = correct + 1
-    total = total + 1
-print("Correct = ", correct, " Total = ", total, " ",((correct/total)*100), "%")
+# Calculate Accuracy
+accuracy_score(y_testing_set, scaled_predictions)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
