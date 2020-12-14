7b9c2d44927356d560bd7cca73379624fb7dde3e
Francisco Coronado <39361813+FCrown@users.noreply.github.com>
2020-12-14 13:32:57 -0800

    Add files via upload
    This script uses billboard 1999-2019 data with spotify data to create and export a csv file with scaled and filtered data that includes  a binary 'hit' column.

---
+++ src/billboard_processing.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 14 09:17:15 2020
+
+@author: Coronado
+@author: Bernard
+"""
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+import numpy as np
+from numpy import asarray
+from sklearn.preprocessing import MinMaxScaler
+import datetime as dt
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+import fitgaussian as fg
+
+#Import the spotify data
+spotify_dat = pd.read_csv('data.csv')
+
+#import the billboard data
+billboard_dat = pd.read_csv('billboardHot100_1999-2019.csv')
+
+#save name of csv file
+savename = "Hits_data_set.csv"
+
+#filter out songs that fall out of 1999-2019 range
+spotify_dat = spotify_dat[spotify_dat['year'].values >= 1999]
+
+hit_data = spotify_dat[spotify_dat['name'].isin(billboard_dat['Name'])]
+
+
+#construct a hit column to add to the scaled dataset
+hit_data['hit'] = 1
+
+#initialize hit column to zero
+spotify_dat['hit'] = 0
+
+#spotify songs that are billboard hits become 1
+spotify_dat['hit'] = spotify_dat['hit'] + hit_data['hit']
+
+#fill in all the nan values with 0
+spotify_dat['hit'] = spotify_dat['hit'].fillna(0)
+
+#prepare spotify data to be normalized
+reduced_spot = spotify_dat.drop(['year','id', 'name','artists','release_date'], axis=1)
+
+
+# Normalize the x features to range 0 to 1.
+scaler = MinMaxScaler();
+scaled_red_spot = scaler.fit_transform(reduced_spot)
+
+#put the data into dataframe with the appropriate labels
+filtered_df = pd.DataFrame(data = scaled_red_spot, columns = reduced_spot.columns)
+
+#rearrange columns
+filtered_df = filtered_df[['valence','acousticness','danceability','duration_ms','energy','explicit',
+                         'instrumentalness','key','liveness','loudness','mode','speechiness','tempo','popularity','hit']]
+
+#export with labels
+filtered_df.to_csv(savename, index = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#hit songs are those that were in billboard top 100 and pass a threshold popularity
+#eliminates any songs that might have had same names 
+# billboard_artists = 
+
+# #only get data with popularity greater than 0.05
+# spot_in_bill = spotify_datafiltered_df['popularity'].values > 0.05]
+
+# Drop uneeded columns from filtered data
+#filt_feat_set = spotify_data.drop(['year','id', 'name','artists','release_date'], axis=1)
+
+# # Normalize the x features to range 0 to 1.
+# scaler = MinMaxScaler();
+# scaled_filt_feat_ds = scaler.fit_transform(filt_feat_set)
+
+# filtered_df = pd.DataFrame(data = scaled_filt_feat_ds, columns = filt_feat_set.columns)
